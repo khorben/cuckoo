2f34ce67832c65bf8fb2b771d188b0b7bc3e5583
Pierre Pronchery <khorben@defora.org>
2013-07-04 03:04:38 +0200

    First functional version (rewritten strongly inspired from vmware.py instead)

--- modules/machinemanagers/xen.py
+++ modules/machinemanagers/xen.py
@@ -3,6 +3,7 @@
 # See the file 'docs/LICENSE' for copying permission.
 
 import logging
+import os.path
 import subprocess
 from pyparsing import OneOrMore, nestedExpr
 
@@ -12,120 +13,140 @@ from lib.cuckoo.common.exceptions import CuckooMachineError
 log = logging.getLogger(__name__)
 
 class Xen(MachineManager):
-    """Virtualization layer for Xen."""
+    """Virtualization layer for Xen using the xm utility."""
 
-    # VM states.
-    RUNNING = "running"
-    POWEROFF = "poweroff"
-    ABORTED = "aborted"
-    ERROR = "machete"
+    def _initialize_check(self):
+        """Check for configuration file and Xen setup.
+        @raise CuckooMachineError: if configuration is missing or wrong.
+        """
+        # Consistency checks.
+        self._check_xen()
+        for machine in self.machines():
+            self._check_machine(machine)
+        # Base checks.
+        super(Xen, self)._initialize_check()
+
+    def _check_xen(self):
+        """Checks a xen configuration file
+        @param filename: path to the configuration file.
+        @raise CuckooMachineError: if the file is not found.
+        """
+        if not self.options.xen.path:
+            self.options.xen.path = '/usr/sbin/xm'
+        if not os.path.exists(self.options.xen.path):
+            raise CuckooMachineError("%s: Invalid path to Xen xm (file not found)" % self.options.xen.path)
+
+    def _check_machine(self, machine):
+        """Checks if a machine exists (including its snapshot).
+        @param machine: the virtual machine.
+        @raise CuckooMachineError: if the snapshot was not found.
+        """
+        filename, snapshot = self._parse_label(machine.label)
+        host = os.path.basename(filename) #XXX use the section name instead
+
+        if not os.path.exists(filename):
+            raise CuckooMachineError("%s: Configuration file not found for machine %s" % (filename, host))
+        if not os.path.exists(snapshot):
+            raise CuckooMachineError("%s: Snapshot not found for machine %s"
+                    % (snapshot, host))
 
     def start(self, label):
         """Start a virtual machine.
         @param label: virtual machine name.
         @raise CuckooMachineError: if unable to start.
         """
-        log.debug("Starting vm %s" % label)
+        filename, snapshot = self._parse_label(label)
+        host = os.path.basename(filename) #XXX use the section name instead
 
-        if self._status(label) == self.RUNNING:
-            raise CuckooMachineError("Trying to start an already started vm %s" % label)
+        # Preventive check
+        if self._is_running(host):
+            raise CuckooMachineError("%s: Machine already running" % host)
 
-        try:
-            subprocess.call([self.options.xen.path, 'create',
-                self.options.xen.filename, 'name=%s' % label],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if proc.returncode != 0:
-                log.debug("Xen returns error checking status for machine %s: %s"
-                                       % (label, err))
-                status = self.ERROR
-        except OSError as e:
-            raise CuckooMachineError("Xen failed starting the machine")
-        self._wait_status(label, self.RUNNING)
+        log.debug("Starting vm %s" % host)
+
+        if not self._command(snapshot, 'restore'):
+            raise CuckooMachineError("%s: Could not start the machine" % host)
 
     def stop(self, label):
         """Stops a virtual machine.
         @param label: virtual machine name.
         @raise CuckooMachineError: if unable to stop.
         """
-        log.debug("Stopping vm %s" % label)
+        filename, snapshot = self._parse_label(label)
+        host = os.path.basename(filename) #XXX use the section name instead
 
-        if self._status(label) in [self.POWEROFF, self.ABORTED]:
-            raise CuckooMachineError("Trying to stop an already stopped vm %s" % label)
+        log.debug("Stopping vm %s" % host)
 
-        try:
-            proc = subprocess.Popen([self.options.xen.path, 'shutdown',
-                label],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if proc.returncode != 0:
-                log.debug("Xen exited with error powering off the machine")
-        except OSError as e:
-            raise CuckooMachineError("Xen failed powering off the machine: %s" % e)
-        self._wait_status(label, [self.POWEROFF, self.ABORTED])
+        if not self._command(host, 'shutdown'):
+            raise CuckooMachineError("%s: Could not stop the machine" % host)
 
-    def _list(self):
-        """Lists virtual machines installed.
-        @return: virtual machine names list.
-        """
-        try:
-            proc = subprocess.Popen([self.options.xen.path, 'list', '-l'],
-                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output, err = proc.communicate()
-        except OSError as e:
-            raise CuckooMachineError("Xen error listing installed machines: %s" % e)
-        if proc.returncode != 0:
-            log.debug("Xen returns error checking status for machine list: %s"
-                    % err)
-            status = self.ERROR
-            raise CuckooMachineError("Xen error listing installed machines: %s" % err)
-
-        machines = []
-        data = OneOrMore(nestedExpr()).parseString(output)
-        for row in data.asList():
-            if row[0] != 'domain':
-                continue
-            for prop in row:
-                if prop[0] == 'name':
-                    machines.append(prop[1])
-        return machines
-
-    def _status(self, label):
-        """Gets current status of a vm.
-        @param label: virtual machine name.
-        @return: status string.
-        """
-        log.debug("Getting status for %s"% label)
-        status = None
-        try:
-            proc = subprocess.Popen([self.options.xen.path, 'list', '-l',
-                label],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            output, err = proc.communicate()
-
-            if proc.returncode != 0:
-                log.debug("Xen returns error checking status for machine %s: %s"
-                                       % (label, err))
-            else:
-                data = OneOrMore(nestedExpr()).parseString(output)
-                for row in data.asList()[0]:
-                    if row[0] == 'status':
-                        if row[1] == '2':
-                            status = self.RUNNING
-                            log.debug("Machine %s status %s" % (label, status))
-                        break
-        except OSError as e:
-            log.warning("Xen failed to check status for machine %s: %s"
-                    % (label, e))
-        # Report back status.
-        if status:
-            self.set_status(label, status)
-            return status
-        else:
-            raise CuckooMachineError("Unable to get status for %s" % label)
 
     def dump_memory(self, label, path):
         """Takes a memory dump.
         @param path: path to where to store the memory dump.
         """
+        filename, snapshot = self._parse_label(label)
+        host = os.path.basename(filename) #XXX use the section name instead
+
+        #FIXME implement with dump-core
         raise CuckooMachineError("Memory dumps are not implemented")
+
+    def _is_running(self, host):
+        """Checks if a virtual machine is running.
+        @param host: name of the virtual machine.
+        @return: running status.
+        """
+        #FIXME use domstate instead
+        try:
+            proc = subprocess.Popen([self.options.xen.path, 'list', '-l',
+                host],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, err = proc.communicate()
+
+            if proc.returncode != 0:
+                log.debug("Xen returns error checking status for machine %s: %s"
+                        % (host, err))
+                return False
+            data = OneOrMore(nestedExpr()).parseString(output)
+            for row in data.asList()[0]:
+                if row[0] == 'status' and row[1] == '2':
+                    return True
+            return False
+        except OSError as e:
+            log.warning("Xen failed to check status for machine %s: %s"
+                    % (label, e))
+            return False
+
+    def _parse_label(self, label):
+        """Parse the label of a virtual machine.
+        @param label: configuration option from the configuration file.
+        @return: tuple of path to the configuration file and snapshot.
+        """
+        opts = label.strip().split(",")
+        if len(opts) != 2:
+            raise CuckooMachineError("Wrong label syntax for %s in xen.conf: %s" % label)
+        filename = opts[0].strip()
+        snapshot = opts[1].strip()
+        return filename, snapshot
+
+    def _command(self, host, command):
+        """Execute a command for a given virtual machine.
+        @param host: name of the virtual machine.
+        @param command: command to execute.
+        @return: if the command was successful
+        """
+        try:
+            proc = subprocess.Popen([self.options.xen.path, command, host],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = proc.communicate()
+            if proc.returncode != 0:
+                log.debug("%s: Could not execute command %s (error code %s: %s)"
+                        % (host, command, proc.returncode, error))
+                return False
+            return True
+        except OSError as e:
+            log.warning("%s: Could not execute command %s"
+                    % (host, command))
+            return False
 
 # vim: et ts=8 sts=4 sw=4
